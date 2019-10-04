1eaf62358d3f65a246be416884324725e1c0cbd1
kontactr <gandhiyash1381997@gmail.com>
2019-10-04 23:18:29 +0530

    Till new updates without notify and remove all previous versions cache

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,20 +1,62 @@
+const current_static_cache = "witter-10"
+
 self.addEventListener('install', function(event) {
   var urlsToCache = [
     '/',
     'js/main.js',
     'css/main.css',
     'imgs/icon.png',
-    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+    //'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW2W8TclTUvlFyQ.woff',
+    //'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff',
+    "imgs/dr-evil.gif"
   ];
 
   event.waitUntil(
     // TODO: open a cache named 'wittr-static-v1'
     // Add cache the urls from urlsToCache
+    
+    caches.open(current_static_cache).then((cache) => {
+      
+      return cache.addAll(urlsToCache)
+    }).catch((err) => {
+      console.log("Error...",err)
+    })
   );
 });
 
 self.addEventListener('fetch', function(event) {
   // Leave this blank for now.
   // We'll get to this in the next task.
+  event.respondWith(caches.open(current_static_cache).then((cache) => {
+    return cache.match(event.request).then((resp) => {
+      
+      if(resp){
+        return resp
+      }else{
+        return fetch(event.request).then((resp) => {
+          
+          if(resp.status == 404){
+            return new Response("No.....")
+          }else{
+            cache.put(event.request , resp.clone())
+            return resp;
+          }
+        }).catch((err) => {
+          return new Response("No.....")
+
+        })
+      }
+    })
+  }))
 });
+
+self.addEventListener("activate" , (event) => {
+  console.log("Activate Start");
+  event.waitUntil(  caches.keys().then((cacheName) => {
+    return Promise.all(cacheName.filter((cache) => {
+      return cache.includes("witter-") && cache != current_static_cache
+    }).map((cache) => {
+       return  caches.delete(cache)
+    }))
+  } ))
+})
